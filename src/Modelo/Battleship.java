387e24b481070d5906eaf2a866c2a4ca5f9ad5cc
damax1995 <davidmax1995@gmail.com>
2017-04-30 22:14:10 +0000

    Gestion de turnos ListaJug->Battleship

--- src/Modelo/Battleship.java
+++ src/Modelo/Battleship.java
@@ -51,7 +51,6 @@ public class Battleship {
         Almacen.getMiAlmacen();
     }
 
-
     private void colocarFlotas() {
         // TODO - implement Battleship.colocarFlotas
         throw new UnsupportedOperationException();
@@ -65,5 +64,22 @@ public class Battleship {
     private void jugar() {
         // TODO - implement Battleship.jugar
         throw new UnsupportedOperationException();
+    }
+
+    public Jugador getJugNoActivo() {
+        if (ListaJugadores.getMyListaJug().getListaJug()[0].isTurno()) {
+            return ListaJugadores.getMyListaJug().getListaJug()[1];
+        }
+        return ListaJugadores.getMyListaJug().getListaJug()[0];
+    }
+
+    public void cambiarJugActivo() {
+        if (ListaJugadores.getMyListaJug().getListaJug()[0].isTurno()) {
+            ListaJugadores.getMyListaJug().getListaJug()[0].setTurno(false);
+            ListaJugadores.getMyListaJug().getListaJug()[1].setTurno(true);
+        } else {
+            ListaJugadores.getMyListaJug().getListaJug()[1].setTurno(false);
+            ListaJugadores.getMyListaJug().getListaJug()[0].setTurno(true);
+        }
     }
 }
